45c24c07a1a4d0a820ffce170eb7b534cb30b3ce
adowns01 <downs.amelia@gmail.com>
2014-08-15 09:52:26 -0700

    finished insertion sort testing

--- lib/test.rb
+++ lib/test.rb
@@ -51,9 +51,16 @@ describe "SelectionSort"do
 end 
 
 describe "InsertionSort"do
-	it "sorts correctly"
-	it "when passed an empty array, it returns an empty array"
-	it "when passed an array with one element, it returns it"
+	it "sorts correctly" do 
+		data = (1..10).to_a
+		expect(InsertionSort.run(data.shuffle)).to eq(data)
+	end 
+	it "when passed an empty array, it returns an empty array" do
+		expect(InsertionSort.run([])).to eq([]) 
+	end 
+	it "when passed an array with one element, it returns it" do 
+		expect(InsertionSort.run([1])).to eq([1]) 
+	end 
 end 
 
 
