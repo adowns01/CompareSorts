f8a32b7524e6636a27e000e628879e2c7ca56ef9
adowns01 <downs.amelia@gmail.com>
2014-08-14 17:28:50 -0700

    refactored selection sort using 2 for loops

--- lib/compareSort.rb
+++ lib/compareSort.rb
@@ -1,32 +1,32 @@
 class CompareSort
-  def self.bubble(data, time = false)
+	def self.bubble(data, time = false)
 
-  	if time 
-  		lam = lambda { BubbleSort.run(data) }
-  		return self.time(lam)
-  	else 
-    	return BubbleSort.run(data)
-    end
+		if time 
+			lam = lambda { BubbleSort.run(data) }
+			return self.time(lam)
+		else 
+			return BubbleSort.run(data)
+		end
 
-  end
+	end
 
-  def self.modBubble(data, time = false)
-  	if time 
-  		lam = lambda { ModifiedBubbleSort.run(data) }
-  		return self.time(lam)
-  	else 
-    	return ModifiedBubbleSort.run(data)
-    end
-    
-  end 
+	def self.modBubble(data, time = false)
+		if time 
+			lam = lambda { ModifiedBubbleSort.run(data) }
+			return self.time(lam)
+		else 
+			return ModifiedBubbleSort.run(data)
+		end
 
-  def self.time(sorting_method)
-  	start_time = Time.now
-  	sorted_list = sorting_method.call 
-  	end_time = Time.now
+	end 
 
-  	return end_time - start_time
-  end 
+	def self.time(sorting_method)
+		start_time = Time.now
+		sorted_list = sorting_method.call 
+		end_time = Time.now
+
+		return end_time - start_time
+	end 
 end
 
 
@@ -38,24 +38,15 @@ class SelectionSort
 		len = data.length
 
 		# iterate through each element in the array
-		data.each_with_index do |datum, datum_index|
-			min_spot = datum_index
-
-			# iterate through all elements past datum
-			(len - datum_index).times do |j|
-				current_index = j + datum_index
-
-				# If found a new min, save the location
-				if data[current_index] < data[min_spot]
-					min_spot = current_index
-				end 
-			end 
-
-			min = data[min_spot]
-			data[min_spot] = datum
-			data[datum_index] = min
-
-
+		for i in 0..len-2
+			min_index = i
+			# iterate through the rest of the array
+			for j in i+1..len-1
+				# if min, save index
+				min_index = j if data[j] < data[min_index]
+			end
+			# put the min in it's correct spot
+			data[i], data[min_index] = data[min_index], data[i]
 		end
 
 		return data
